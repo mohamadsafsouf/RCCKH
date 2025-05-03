6b168de9be5053ff607d6b09050600dbfdd6ec22
Mohamad Safsouf <msafsouf05@gmail.com>
2025-05-03 23:17:59 +0300

    dhn

--- Package.swift
+++ Package.swift
@@ -1,38 +1,27 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "KeyGeneratorCC",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "KeyGeneratorCC", targets: ["KeyGeneratorCC"])
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
-        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1")
     ],
     targets: [
         .executableTarget(
             name: "KeyGeneratorCC",
             dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "NIOCore", package: "swift-nio"),
-                .product(name: "NIOPosix", package: "swift-nio"),
-            ],
-            swiftSettings: swiftSettings
+                .product(name: "Vapor", package: "vapor")
+            ]
         ),
         .testTarget(
             name: "KeyGeneratorCCTests",
-            dependencies: [
-                .target(name: "KeyGeneratorCC"),
-                .product(name: "VaporTesting", package: "vapor"),
-            ],
-            swiftSettings: swiftSettings
+            dependencies: ["KeyGeneratorCC"]
         )
     ]
 )
-
-var swiftSettings: [SwiftSetting] { [
-    .enableUpcomingFeature("ExistentialAny"),
-] }
